61ab8cfb255c41ba59075bbbb7aeae3c59896a81
amit4alljava <amit4alljava@gmail.com>
2018-03-12 15:58:00 +0530

    Code

---
+++ A.java
@@ -0,0 +1,15 @@
+
+public class A {
+//A(){
+//	System.out.println("A Default Cons");
+//}
+A(int x){
+	System.out.println("A Param Cons");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A obj = new A(90);
+
+	}
+
+}

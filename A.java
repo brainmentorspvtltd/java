ace9ce41475b16d529a841663242ea1793ee00f6
Amit Srivastava <amit.srivastava4all@gmail.com>
2018-04-14 22:55:29 +0530

    Update A.java

--- A.java
+++ A.java
@@ -6,6 +6,9 @@ public class A {
 A(int x){
 	System.out.println("A Param Cons");
 }
+	void show(){
+	System.out.println("show");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		A obj = new A(90);
